27e87d3e50b33f35ed03dd6680c8784435cd0399
Jared Wyatt <j@wyatt.co>
2012-08-13 10:50:27 -0700

    added bot config file, responder when tnyubot gets opped

--- bot.rb
+++ bot.rb
@@ -1,4 +1,9 @@
 require 'cinch'
+require './lib/ConfigData'
+
+# :production or :test
+# (this determines the nick of the bot and the channels it joins)
+ENVIRONMENT = :test
 
 # Automatically include all available plugins
 $plugins = []
@@ -11,12 +16,27 @@ end
 
 # Bot configuration
 bot = Cinch::Bot.new do
+  config = ConfigData.new :bot_config
+  raise config.error unless config.has_data?
+  config = config.hash[ENVIRONMENT]
+  
   configure do |c|
-    c.nick = 'tnyubot'
-    c.server = 'irc.freenode.org'
-    c.channels = ['#tech@nyu']
+    c.server = config[:server]
+    c.channels = config[:channels]
+    c.nick = config[:nick]
+    if config.has_key? :username
+      c.sasl.username = config[:username]
+      c.sasl.password = ENV['IRC_PASSWORD']
+    end
     c.plugins.plugins = $plugins
     c.plugins.prefix = nil
+  end
+  
+  on :op do |m|
+    if m.user == bot_name
+      msgs = ConfigData.new :op_msgs, {do_parse: false}
+      m.reply msgs.array[rand 0..(msgs.array.count-1)] if msgs.has_data?
+    end
   end
 end
 
